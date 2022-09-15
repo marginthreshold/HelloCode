f42f127719be7e0278d5213fa9872c1883e1447e
Lavanov <alavanov@rencap.com>
2022-09-15 10:45:47 +0300

    Example_Condition project

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, привет МАША!!!");
+}
+else
+{
+    Console.Write("Привет, "+ username);
+}

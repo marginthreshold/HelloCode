0d3c20a140b85e401face8ab00a7abfa87c2cffa
Lavanov <alavanov@rencap.com>
2022-09-19 12:21:02 +0300

    Added Fill and Print Array methods

---
+++ Example009_AutoFillArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+void FillArray(int[] colleciton)
+{
+    int lenght = colleciton.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        colleciton[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position =0;
+    while(position<count)
+    {
+        WriteLine(coll[position]);
+        position++;
+
+    }
+}
+
+int[] array=new int[10];
+
+FillArray(array);
+PrintArray(array);
